59204fc06a619f88a75245931b382fd549dd2dc4
guguxl <guguxl@sina.com>
2016-08-11 14:10:40 +0800

    project startup

---
+++ src/main/java/com/guxl/upload/UploadFileManager.java
@@ -0,0 +1,12 @@
+package com.guxl.upload;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UploadFileManager {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UploadFileManager.class, args);
+	}
+}
---
+++ src/test/java/com/guxl/upload/UploadFileManagerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.guxl.upload;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class UploadFileManagerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
